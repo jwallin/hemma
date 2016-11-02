19542af68a1cd001aba69009b49c296df22041c5
Johan Wallin <j.e.wallin@gmail.com>
2016-11-02 23:05:11 +0100

    Fix app start with babel-cli

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
 var NodeCEC = require('nodecec')
 import {notification} from './config/notification';
 
-import irUtil from './utils/ir-command';
-import IRService from './services/ir';
-import NotificationService from './services/notification';
-import InputController from './controllers/input';
-import CECController from './controllers/cec';
+import irUtil from './app/utils/ir-command';
+import IRService from './app/services/ir';
+import NotificationService from './app/services/notification';
+import InputController from './app/controllers/input';
+import CECController from './app/controllers/cec';
 
-var cec = new NodeCEC();
+const cec = new NodeCEC();
 
-var irService = new irService(irUtil);
-var inputController = new InputController()
-var cecController = new CECController()
+const irService = new IRService(irUtil);
+const notificationService = new NotificationService(notification);
+const inputController = new InputController(irService, notificationService)
+const cecController = new CECController(cec, inputController);
+
+cecController.start();
